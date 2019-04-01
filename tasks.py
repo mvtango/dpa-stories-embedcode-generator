a0a27c6be064cc90e6508bb0ada1f97fb41a3a30
Martin Virtel <martin.virtel@gmail.com>
2019-04-01 15:23:05 +0200

    livereload

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import task
+import os
+
+HERE = os.path.split(__file__)[0]
+
+PWD = os.environ.get("PWD")
+MYDIR = os.path.split(PWD)[1]
+
+
+@task
+def sync(c):
+    c.run(f"""
+        cd {HERE}/htdocs
+        aws --profile=martin.virtel@dpa-info.com --region=eu-central-1 \
+            s3 sync --acl=public-read  \
+            ./ s3://dpa-newslab-prototype-webspace/dpa-stories/embed/ --exclude='node_modules/*'
+    """)
+
+
+@task
+def serve(c):
+    c.run(f"""
+        livereload htdocs/
+    """)
+
+
+@task
+def introspect(c):
+    from IPython import embed
+    embed()
+
+
